400f83ceb95b3355c6c815bac7bc24d008997ba7
pranay595 <57897846+pranay595@users.noreply.github.com>
2022-03-05 17:40:25 +0530

    Time: 3 ms (85.44%), Space: 7.9 MB (65.18%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -3,10 +3,9 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> game = {};
         for(int i=1;i<=n;i++){
-            if(i%3==0){
-                if(i%5==0)
+            if(i%5==0 && i%3==0)
                     game.push_back("FizzBuzz");
-                else
+            else if(i%3==0){
                     game.push_back("Fizz");
             }
             else if(i%5==0){

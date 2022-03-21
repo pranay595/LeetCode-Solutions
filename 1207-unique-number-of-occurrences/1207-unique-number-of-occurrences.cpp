5ebcc0a141cd83649d855aa379f6e52894db380d
pranay595 <57897846+pranay595@users.noreply.github.com>
2022-03-21 10:34:54 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (73.56%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,16 +3,15 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         int n = arr.size();
         unordered_map<int,int> mp;
-        unordered_map<int,int> sol;
+        set<int> sol;
         for(int i=0;i<n;i++){
             ++mp[arr[i]];
         }
         for(auto i:mp){
-            ++sol[i.second];
-        }
-        for(auto i:sol){
-            if(i.second>1)
+            if(sol.count(i.second))
                 return false;
+            else
+                sol.insert(i.second);
         }
         return true;
     }

3f77600e32cf1ab19827765553743ae795e27246
pranay595 <57897846+pranay595@users.noreply.github.com>
2022-03-23 23:49:41 +0530

    Time: 20 ms (43.68%), Space: 15.8 MB (82.88%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -4,12 +4,12 @@ public:
         unordered_map<int,int> mp;
         vector<int> ans;
         int n = nums.size();
+        int major = n/3;
         
         for(int i=0;i<n;i++){
             ++mp[nums[i]];
         }
         
-        int major = n/3;
         
         for(auto i:mp){
             if(i.second>major)

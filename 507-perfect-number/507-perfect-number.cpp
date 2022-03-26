9d2d5fe9b85f2ea4bdd96bcf768f460861b1adf7
pranay595 <57897846+pranay595@users.noreply.github.com>
2022-03-26 20:47:17 +0530

    Time: 1274 ms (32.01%), Space: 5.8 MB (89.80%) - LeetHub

---
+++ 507-perfect-number/507-perfect-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count = 0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0)
+                count+=i;
+        }
+        if(count==num)
+            return true;
+        return false;
+    }
+};

205ff3ba017bf773c416471ca7e9b4eb17a34eca
pranay595 <57897846+pranay595@users.noreply.github.com>
2022-03-29 14:33:23 +0530

    Time: 6 ms (16.91%), Space: 8.3 MB (24.55%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            ++mp[nums[i]];
+        }
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for(auto i:mp){
+            if(i.first==0)
+                a = i.second;
+            else if(i.first==1)
+                b = i.second;
+            else
+                c = i.second;
+        }
+        
+        int i=0;
+        
+        while(a && i<n){
+            nums[i]=0;
+            a--;
+            i++;
+        }
+        
+        while(b && i<n){
+            nums[i]=1;
+            b--;
+            i++;
+        }
+        
+        while(c && i<n){
+            nums[i]=2;
+            c--;
+            i++;
+        }
     }
 };

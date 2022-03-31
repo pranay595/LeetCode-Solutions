7287cdd71c94482e3b941eeb69e0d24fc55b2886
pranay595 <57897846+pranay595@users.noreply.github.com>
2022-03-31 09:32:24 +0530

    Time: 13 ms (48.70%), Space: 9.9 MB (92.88%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            ++mp[nums[i]];
+         int n = nums.size();
+        
+        long long a = nums[0];
+        
+        for(int i=1;i<n;i++){
+            a^=nums[i];
         }
         
-        vector<int> ans ;
-        for(auto i:mp){
-            if(i.second==1)
-                ans.push_back(i.first);
+        int rightSetBit = (a & ~(a-1));
+        
+        int first = 0;
+        int second = 0;
+        
+        for(int i=0;i<n;i++){
+            if((nums[i] & rightSetBit) !=0)
+                first^=nums[i];
+            else
+                second^=nums[i];
         }
-        return ans;
+        
+            return {first,second};
+        
+    
     }
 };

9f9ef84ec514ca040101dfe831ae9dcf11a5e7f0
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-05 20:04:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.00%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double ans=1;
+        if(n<0){
+            x=1/x;
+        }
         while(n){
             if((n&1)!=0)
-                ans=n>0? ans*x:ans/x;
+                ans=ans*x;
             x=x*x;
             n=n/2;
         }

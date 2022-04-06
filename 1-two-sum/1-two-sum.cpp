2e29c26ca6a66e5f18931a859434b16fe5fe9b31
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-06 09:40:28 +0530

    Time: 21 ms (54.79%), Space: 10.8 MB (44.73%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,19 +5,16 @@ public:
         if(n==2){
             return {0,1};
         }
-        vector<int> ans;
         unordered_map<int,int> mp;
         
         for(int i=0;i<n;i++){
             if(mp.count(target-nums[i])){
-                ans.push_back(mp[target-nums[i]]);
-                ans.push_back(i);
-                return ans;
+                return {mp[target-nums[i]],i};
             }
             else{
                 mp[nums[i]]=i;
             }
         }
-        return ans;
+        return {};
     }
 };

b4df61c1a64a4a4200868fb4eb3a02d07eb35a10
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-06 20:49:26 +0530

    Time: 91 ms (18.07%), Space: 9.7 MB (40.72%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<vector<int>> &ans, int l, int r){
+    void helper(vector<int>& nums, set<vector<int>> &st, int l, int r){
         if(l==r){
-            ans.push_back(nums);
+            st.insert(nums);
             return;
         }
         
         for(int i=l;i<=r;i++){
             swap(nums[l],nums[i]);
-            helper(nums,ans,l+1,r);
+            helper(nums,st,l+1,r);
             swap(nums[l],nums[i]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        helper(nums,ans,0,nums.size()-1);
         
         set<vector<int>> st;
+        helper(nums,st,0,nums.size()-1);
+        
+        
         /* unordered_set<vector<int>> st will give an error, I found reason for it on stacoverflow --
         It's because unordered_set is using std::hash template to compute hash for its entries and there is no std::hash for pairs. You have to define custom hash to use unordered_set.
         */ 
-        int n = ans.size();
         
-        for(int i=0;i<n;i++){
-            st.insert(ans[i]);
-        }
-        
-        ans.clear();
+        vector<vector<int>> ans;
         
         for(auto i:st){
             ans.push_back(i);

5b7d71a1cae35c93f027604c6f04783a731e4f66
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-07 09:03:54 +0530

    Time: 2 ms (56.85%), Space: 7.6 MB (34.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        int end = n-1;
+        
+        sort(stones.begin(),stones.end());
+        
+        while(end>0){
+            int st1 = stones[end];
+            int st2 = stones[end-1];
+            
+            stones.pop_back();
+            stones.pop_back();
+            
+            if(st1!=st2){
+                stones.push_back(st1-st2);
+                end--;
+            }else{
+                end-=2;
+            }
+            sort(stones.begin(),stones.end());
+        }
+        if(stones.empty())
+            return 0;
+        
+        return stones[0];
+    }
+};

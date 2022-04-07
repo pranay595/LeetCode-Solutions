60502dd56891ca7286d18fe75890739dbc059bfe
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-08 00:06:47 +0530

    Time: 6 ms (15.31%), Space: 7.3 MB (59.68%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,11 +4,10 @@ public:
         int n = nums.size();
         int count = 0;
         
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j])
-                    count++;
-            }
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<n;i++){
+            count+=mp[nums[i]]++;
         }
         
         return count;

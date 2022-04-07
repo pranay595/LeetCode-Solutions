094144fedd0671255e5a3d2da928ede260036842
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-08 00:26:57 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (24.17%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ch='a';
-        int sum1 =0;
-        int sum2 = 0;
+        int sum =0;
         for(int i=0;i<s.size();i++){
-            sum1+=s[i];
+            t[i+1] += (t[i]-s[i]);
         }
-        for(int i=0;i<t.size();i++){
-            sum2+=t[i];
-        }
-        int st = 'a'-s[0];
-        cout<<st<<endl;
-        cout<<sum2-sum1<<endl;
-        ch = sum2-sum1;
-        
-        return ch;
+        return t[s.size()];
     }
 };

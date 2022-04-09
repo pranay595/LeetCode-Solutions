34f780e63de5ef1a3195ce57912ec7d3e83c7eea
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-09 18:39:05 +0530

    Time: 15 ms (45.96%), Space: 10.7 MB (88.60%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    
-    void helper(int ind, int target, int n, vector<int>& ds, vector<int>& candidates, vector<vector<int>>& ans){
+    void helper(int ind, int target, int n, vector<int>& candidates, vector<int>& ds, vector<vector<int>>& ans){
         if(ind==n){
-            if(target==0){
+            if(target==0)
                 ans.push_back(ds);
-            }
             return;
         }
+        
         if(candidates[ind]<=target){
-        ds.push_back(candidates[ind]);
-        helper(ind,target-candidates[ind],n,ds,candidates,ans);
-        ds.pop_back();
+            ds.push_back(candidates[ind]);
+            helper(ind,target-candidates[ind],n,candidates,ds,ans);
+            ds.pop_back();
         }
-        helper(ind+1,target,n,ds,candidates,ans);
         
+        helper(ind+1,target,n,candidates,ds,ans);
     }
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>ds;
+        vector<int> ds;
         vector<vector<int>> ans;
-        
         int n = candidates.size();
         
-        helper(0,target,n,ds,candidates,ans);
+        helper(0,target,n,candidates,ds,ans);
         
         return ans;
     }

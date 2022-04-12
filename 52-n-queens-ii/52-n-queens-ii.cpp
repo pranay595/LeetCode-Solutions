ba81118506536a8ec4a40f8cb9d32cf4c3c0dd7e
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-12 23:22:30 +0530

    Time: 6 ms (61.71%), Space: 6.2 MB (64.61%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,57 +1,36 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string>& board, int n){
-        for(int i=0;i<col;i++){
-            if(board[row][i]=='Q')
-                return false;
-        }
-        
-        int x = row;
-        int y = col;
-        while(x>=0 && y>=0){
-            if(board[x][y]=='Q')
-                return false;
-            y--;
-            x--;
-        }
-        
-        x = row;
-        y = col;
-        while(x<n && y>=0){
-            if(board[x][y]=='Q')
-                return false;
-            x++;
-            y--;
-        }
-        
-        return true;
-    }
     
-    void solve(int col, vector<string>& board, int n, int& count){
+    void solve(int col, vector<int>& board, int n, int& count, vector<int>& rows, vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
         if(col==n){
             count++;
             return;
         }
         
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
-                board[row][col]='Q';
-                solve(col+1,board,n,count);
-                board[row][col]='.';
+            if(rows[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
+                
+                rows[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1+col-row] = 1;
+                
+                solve(col+1,board,n,count,rows,upperDiagonal,lowerDiagonal);
+                
+                rows[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row] = 0;
             }
         }
     }
     
     int totalNQueens(int n) {
-        vector<string> board(n);
+        vector<int> board(n,0);
         
-        string s(n,'.');
-        for(int i=0;i<n;i++){
-            board[i]=s;
-        }
         int count = 0;
         
-        solve(0,board,n,count);
+        vector<int> rows(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
+        
+        solve(0,board,n,count,rows,upperDiagonal,lowerDiagonal);
         
         return count;
     }

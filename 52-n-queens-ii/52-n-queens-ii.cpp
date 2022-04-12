301333069ac79b97b3ac46002636291e5d628e17
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-12 23:24:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (80.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void solve(int col, vector<int>& board, int n, int& count, vector<int>& rows, vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
+    void solve(int col, int n, int& count, vector<int>& rows, vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
         if(col==n){
             count++;
             return;
@@ -14,7 +14,7 @@ public:
                 lowerDiagonal[row+col] = 1;
                 upperDiagonal[n-1+col-row] = 1;
                 
-                solve(col+1,board,n,count,rows,upperDiagonal,lowerDiagonal);
+                solve(col+1,n,count,rows,upperDiagonal,lowerDiagonal);
                 
                 rows[row] = 0;
                 lowerDiagonal[row+col] = 0;
@@ -24,13 +24,12 @@ public:
     }
     
     int totalNQueens(int n) {
-        vector<int> board(n,0);
         
         int count = 0;
         
         vector<int> rows(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
         
-        solve(0,board,n,count,rows,upperDiagonal,lowerDiagonal);
+        solve(0,n,count,rows,upperDiagonal,lowerDiagonal);
         
         return count;
     }

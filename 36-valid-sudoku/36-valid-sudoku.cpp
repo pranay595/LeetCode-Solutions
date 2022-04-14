b86777e661ffc3c7749a797931f8dd73c56ebf69
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-14 16:28:40 +0530

    Time: 30 ms (57.62%), Space: 19.1 MB (54.30%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool helper(int row, int col, vector<vector<char>>& board){
+        unordered_set<char> st;
+        
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+            char curr = board[i][j];
+            if(curr!='.'){    
+            if(st.count(curr))
+                return false;
+            st.insert(curr);
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> st;
+        
+        for(int i=0;i<9;i++){
+            st.clear();
+            for(int j=0;j<9;j++){
+                char curr = board[i][j];
+                if(curr!='.'){
+                    if(st.count(curr))
+                        return false;
+                    st.insert(curr);
+                }
+            }
+        }
+        
+        st.clear();
+        
+         for(int i=0;i<9;i++){
+            st.clear();
+            for(int j=0;j<9;j++){
+                char curr = board[j][i];
+                if(curr!='.'){
+                    if(st.count(curr))
+                        return false;
+                    st.insert(curr);
+                }
+            }
+        }
+        
+        st.clear();
+        
+         for(int i=0;i<9;i+=3){
+            st.clear();
+            for(int j=0;j<9;j+=3){
+                if(!helper(i,j,board))
+                    return false;
+            }
+        }
+            return true;
+    }
+};

65b34c169e1662fee702e1b39c17aea679380815
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-15 00:06:33 +0530

    Time: 150 ms (65.26%), Space: 57.6 MB (60.89%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int st, int end){
+        while(st<=end){
+            if(s[st++]!=s[end--])
+                return false;
+        }
+        return true;
+    }
+    
+    void helper(int ind, int n, vector<string>& path, vector<vector<string>>& ans, string s){
+        if(ind==s.size()){
+            ans.push_back(path);
+            return;
+        }
+        
+        for(int i=ind;i<s.size();i++){
+            if(isPalindrome(s,ind,i)){
+                path.push_back(s.substr(ind,i-ind+1));
+                helper(i+1,n,path,ans,s);
+                path.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<string> path;
+        vector<vector<string>> ans;
+        
+        int n = s.size();
+        
+        helper(0,n,path,ans,s);
+        
+        return ans;
+    }
+};

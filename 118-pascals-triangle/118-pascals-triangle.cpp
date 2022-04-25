462bbae6b15237f2483aa57477f2518833e8f208
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-25 16:56:57 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.59%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,15 +4,16 @@ public:
         vector<vector<int>> ans;
         if(numRows==1)
             return {{1}};
+        
         for(int i=0;i<numRows;i++){
             vector<int> row;
             row.push_back(1);
-            for(int j=1;j<i;j++){
-                row.push_back(ans[i-1][j]+ans[i-1][j-1]);
-            }
+            for(int j=1;j<i;j++)
+                row.push_back(ans[i-1][j-1]+ans[i-1][j]);
             if(i>0) row.push_back(1);
             ans.push_back(row);
         }
+        
         return ans;
     }
 };

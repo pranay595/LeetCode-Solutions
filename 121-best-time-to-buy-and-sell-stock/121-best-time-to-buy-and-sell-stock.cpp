7d583f9bbde508f96b1e1e6df0e63df7bd356360
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-25 18:23:09 +0530

    Time: 174 ms (44.05%), Space: 93.3 MB (90.24%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,10 +4,10 @@ public:
         int minPrice = INT_MAX;
         int profit = 0;
         int n = prices.size();
-       for(int i=0;i<n;i++){
-           minPrice = min(prices[i],minPrice);
-           profit = max(prices[i]-minPrice,profit);
-       }
+        for(int i=0;i<n;i++){
+            minPrice = min(prices[i],minPrice);
+            profit = max(prices[i]-minPrice,profit);
+        }
         return profit;
     }
 };

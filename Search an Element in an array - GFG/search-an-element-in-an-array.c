286b95b068780869f38beb820dbee54ae075161a
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-27 01:10:08 +0530

    Added solution - LeetHub

---
+++ Search an Element in an array - GFG/search-an-element-in-an-array.c
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int search(int arr[], int n, int X)
+{
+        
+    // Your code here
+    for(int i=0;i<n;i++){
+        if(arr[i]==X)
+            return i;
+    }
+    
+    return -1;
+        
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    scanf("%d", &testcases);
+    while(testcases--)
+    {
+        int sizeOfArray;
+        scanf("%d", &sizeOfArray);
+        int arr[sizeOfArray];
+        int x;
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        scanf("%d", &x);
+        printf("%d\n", search(arr,sizeOfArray,x)); //Linear search
+    }
+
+    return 0;
+    
+}
+  // } Driver Code Ends

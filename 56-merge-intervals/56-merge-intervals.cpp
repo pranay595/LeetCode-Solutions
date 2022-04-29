c6adb8248fa1c55bc444aa338dff9254382de2a2
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-04-29 08:00:12 +0530

    Time: 60 ms (32.54%), Space: 20.2 MB (17.81%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
-        
         vector<int> tempInterval = intervals[0];
+        
         for(auto i:intervals){
             if(i[0]<=tempInterval[1]){
-                tempInterval[1] = max(tempInterval[1],i[1]);
+                tempInterval[1] = max(i[1],tempInterval[1]);
             }
             else{
                 ans.push_back(tempInterval);
@@ -15,6 +15,7 @@ public:
             }
         }
         ans.push_back(tempInterval);
+        
         return ans;
     }
 };

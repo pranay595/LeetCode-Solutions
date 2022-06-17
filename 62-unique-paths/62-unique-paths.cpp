df11ff48d6a07612cdab7788ccc04f0093791b96
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-06-17 15:31:15 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (56.41%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -16,7 +16,29 @@ public:
         return dp[i][j] = left+up;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // return helper(m-1,n-1,dp);
+        
+        vector<int> prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    temp[j]=1;
+                    continue;
+                }
+                int left = 0;
+                int up = 0;
+                if(i>0)
+                    up = prev[j];
+                if(j>0)
+                    left = temp[j-1];
+                    temp[j] = up + left;
+                
+            }
+            prev = temp;
+        }
+        
+        return prev[n-1];
     }
 };

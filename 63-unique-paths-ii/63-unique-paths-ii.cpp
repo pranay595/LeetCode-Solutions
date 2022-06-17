1eea83f77a826847e6441fdecb98647550d767f1
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-06-17 18:48:12 +0530

    Time: 7 ms (34.89%), Space: 8 MB (12.44%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -26,31 +26,33 @@ int helper(int i, int j, vector<vector<int>> &dp, vector<vector<int>>& obstacleG
         
         vector<vector<int>> dp(m,vector<int>(n,-1));
         // return helper(m-1,n-1,dp,obstacleGrid);
+        vector<int> prev(n,0);
         
         for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
             for(int j=0;j<n;j++){
                 if(obstacleGrid[i][j]==1)
-                    dp[i][j]=0;
-                
+                    temp[j]=0;
+                    
                 else if(i==0 && j==0)
-                    dp[i][j]=1;
-                
+                    temp[j]=1;
                 
                 else{
                 int up = 0;
                 int left = 0;
                 
                 if(i>0){
-                    up = dp[i-1][j];
+                    up = prev[j];
                 }
                 if(j>0)
-                    left = dp[i][j-1];
+                    left = temp[j-1];
                 
-                dp[i][j]=left+up;
+                temp[j]=left+up;
                 }
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
         
     }
 };

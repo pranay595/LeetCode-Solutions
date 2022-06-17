70506725c2776112d36e3e3ae6324e0afb7abf24
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-06-17 22:28:50 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (7.72%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -17,6 +17,20 @@ public:
         int m = triangle[n-1].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
         
-        return helper(0,0,n,triangle,dp);
+//         return helper(0,0,n,triangle,dp);
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                
+                dp[i][j] = min(down,diag);
+            }
+        }
+        return dp[0][0];
+        
     }
 };

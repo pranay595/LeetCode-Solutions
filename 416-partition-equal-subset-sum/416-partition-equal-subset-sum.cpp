a737145da8b7dfc510c277964db341469056588e
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2022-06-21 17:03:49 +0530

    Time: 396 ms (30.33%), Space: 75.6 MB (20.82%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -16,7 +16,27 @@ public:
             pick = helper(ind-1,target-nums[ind],nums,dp);
         
         return dp[ind][target] = pick || notPick;
+    
     }
+    
+    bool can(int n, int target, vector<int>& nums){
+        vector<bool> prev(target+1,0), curr(target+1,0);
+        if(nums[0]<=target)
+            prev[nums[0]] = true;
+        
+        for(int ind = 1;ind<n;ind++){
+            for(int k=1;k<=target;k++){
+                bool notPick = prev[k];
+                bool pick = false;
+                if(nums[ind]<=k)
+                    pick = prev[k-nums[ind]];
+                curr[k] = pick || notPick;
+            }
+            prev = curr;
+        }
+        return prev[target];
+    }
+    
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
@@ -27,6 +47,7 @@ public:
             return false;
         sum/=2;
         vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-        return helper(n-1,sum,nums,dp);
+        // return helper(n-1,sum,nums,dp);
+        return can(n,sum,nums);
     }
 };
